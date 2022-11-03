7ee81e2899061b357429a42ce48441bf75a9f59a
Crowdbotics <engineering@crowdbotics.com>
2022-11-03 07:27:11 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/testfigma_dev_42555/MainActivity.java
@@ -0,0 +1,15 @@
+package com.testfigma_dev_42555;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "testfigma_dev_42555";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "testfigma_dev_42555",
+  url: "https://testfigma_dev_42555.botics.co",
+  api: "https://testfigma_dev_42555.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
